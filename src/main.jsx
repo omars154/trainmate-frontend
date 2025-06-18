3547e07aa6d80ccbf7d69ffd826b3ea901321d6d
omars154 <omarsaifan154@gmail.com>
2025-06-18 17:09:00 +0300

    Getting sick of this

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import AuthProviderWrapper from './AuthProviderWrapper.jsx';
 import { UserProvider } from './utils/UserContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProviderWrapper>
       <UserProvider>
         <App />
       </UserProvider>
-    </AuthProviderWrapper>
   </BrowserRouter>
 );
 
